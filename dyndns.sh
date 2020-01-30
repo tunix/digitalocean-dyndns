6cba88c6d05bd2804f8549d291d359e67dd36e28
Evgenii Vilkov <zzeneg@gmail.com>
2020-01-30 13:38:37 +0000

    add support for multiple subdomains

--- dyndns.sh
+++ dyndns.sh
@@ -19,25 +19,29 @@ while ( true ); do
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
         $dns_list)
-    record_id=$(echo $domain_records| jq ".domain_records[] | select(.type == \"A\" and .name == \"$NAME\") | .id")
-    record_data=$(echo $domain_records| jq -r ".domain_records[] | select(.type == \"A\" and .name == \"$NAME\") | .data")
-    
-    test -z $record_id && die "No record found with given domain name!"
 
     ip="$(curl -s ipinfo.io/ip)"
-    data="{\"type\": \"A\", \"name\": \"$NAME\", \"data\": \"$ip\"}"
-    url="$dns_list/$record_id"
 
     if [[ -n $ip ]]; then
-        if [[ "$ip" != "$record_data" ]]; then
-            echo "existing DNS record address ($record_data) doesn't match current IP ($ip), sending data=$data to url=$url"
+        for sub in ${NAME//;/ }; do
+            record_id=$(echo $domain_records| jq ".domain_records[] | select(.type == \"A\" and .name == \"$sub\") | .id")
+            record_data=$(echo $domain_records| jq -r ".domain_records[] | select(.type == \"A\" and .name == \"$sub\") | .data")
+            
+            test -z $record_id && echo "No record found with '$sub' domain name!" && continue
 
-            curl -s -X PUT \
-                -H "Content-Type: application/json" \
-                -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
-                -d "$data" \
-                "$url" &> /dev/null
-        fi
+            if [[ "$ip" != "$record_data" ]]; then
+                data="{\"type\": \"A\", \"name\": \"$sub\", \"data\": \"$ip\"}"
+                url="$dns_list/$record_id"
+
+                echo "existing DNS record address ($record_data) doesn't match current IP ($ip), sending data=$data to url=$url"
+
+                curl -s -X PUT \
+                    -H "Content-Type: application/json" \
+                    -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
+                    -d "$data" \
+                    "$url" &> /dev/null
+            fi
+        done
     else
         echo "IP wasn't retrieved within allowed interval. Will try $sleep_interval seconds later.."
     fi
